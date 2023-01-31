0380ca3f7d5c2a5369e6332b656f8850af5634ac
Suman Adhya <51852383+AdhyaSuman@users.noreply.github.com>
2023-01-31 17:43:52 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,9 +7,6 @@ from setuptools import setup, find_packages
 with open('README.rst') as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
-    history = history_file.read()
-
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
